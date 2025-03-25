365c334f71ee4f15cbd7f5337a4ea87aa62cc5ef
Klaas Pieter Annema <kp@annema.me>
2025-03-25 14:48:31 +0100

    Use nunjucks for everything

---
+++ eleventy.config.js
@@ -0,0 +1,11 @@
+export default async function (eleventyConfig) {
+  return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+}
