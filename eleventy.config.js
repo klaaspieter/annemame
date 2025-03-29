e4f860143aa2db945fca364f6806899304e1f78a
Klaas Pieter Annema <kp@annema.me>
2025-03-29 16:57:58 +0100

    Add brb and health-shortcuts pages

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,11 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 export default async function (eleventyConfig) {
   eleventyConfig.addBundle("css");
+
+  eleventyConfig.addWatchTarget("src/**/*.{svg,webp,png,jpg,jpeg,gif}");
+
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 
   return {
     markdownTemplateEngine: "njk",
